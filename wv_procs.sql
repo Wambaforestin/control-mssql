6706974ac4f277e2ea77b9675f8b211b627f9fe6
Jose DA SILVA <60261740+joshd97@users.noreply.github.com>
2025-03-21 12:52:11 +0100

    Update wv_procs.sql
    fix erros

--- wv_procs.sql
+++ wv_procs.sql
@@ -6,20 +6,20 @@ BEGIN
     DECLARE @tour_count INT;
     DECLARE @max_tours INT;
     
-    -- Récupérer le nombre maximum de tours pour cette partie
+    -- Récupération du nombre maximum de tours pour cette partie
     SELECT @max_tours = COUNT(*) FROM turns WHERE id_party = @PARTY_ID;
     
-    -- Initialiser le compteur à 0
+    -- Initiation du compteur à 0
     SET @tour_count = 0;
     
-    -- Créer autant de tours que nécessaire
+    -- Création de plusieurs tours que nécessaire
     WHILE @tour_count < @max_tours
     BEGIN
-        -- Créer un nouveau tour
+        -- Créeation d'un nouveau tour
         INSERT INTO turns (id_party, start_time, end_time)
         VALUES (@PARTY_ID, GETDATE(), DATEADD(MINUTE, 5, GETDATE()));
         
-        -- Incrémenter le compteur
+        -- Incrémentation du compteur
         SET @tour_count = @tour_count + 1;
     END;
 END;
@@ -40,7 +40,7 @@ BEGIN
     DECLARE @target_position_col VARCHAR(255);
     DECLARE @target_position_row VARCHAR(255);
     
-    -- Récupérer les actions des joueurs pour ce tour
+    -- Récupération des actions des joueurs pour ce tour
     DECLARE actions CURSOR FOR
     SELECT 
         id_player,
@@ -52,25 +52,25 @@ BEGIN
     FROM players_play
     WHERE id_turn = @TOUR_ID;
     
-    -- Ouvrir le curseur
+    -- Ouverture du curseur
     OPEN actions;
     
-    -- Lire la première ligne
+    -- Lecture de la première ligne
     FETCH NEXT FROM actions INTO @player_id, @action, @origin_position_col, @origin_position_row, @target_position_col, @target_position_row;
     
-    -- Tant qu'il y a des lignes à lire
+    -- si il y'a d'autre ligne
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        -- Mettre à jour la position du joueur
+        -- Mise à jour de la position du joueur
         UPDATE players_play
         SET end_time = GETDATE()
         WHERE id_player = @player_id AND id_turn = @TOUR_ID;
         
-        -- Lire la ligne suivante
+        -- Lire la ligne suivant
         FETCH NEXT FROM actions INTO @player_id, @action, @origin_position_col, @origin_position_row, @target_position_col, @target_position_row;
     END;
     
-    -- Fermer le curseur
+    -- terminer l'action du curseur 
     CLOSE actions;
     DEALLOCATE actions;
 END;
@@ -82,7 +82,7 @@ test de la procédure
 CREATE PROCEDURE USERNAME_TO_LOWER
 AS
 BEGIN
-    -- Mettre les noms des joueurs en minuscule
+    --utilisation des polices minuscules 
     UPDATE players
     SET pseudo = LOWER(pseudo);
 END;
